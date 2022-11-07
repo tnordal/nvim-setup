dba3142d800aafc46d792f6c74b2ea547e85e467
Tom Nordal <tnordal@gmail.com>
2022-11-07 06:12:33 +0100

    add all lua configuration files

---
+++ config/lua/keys.lua
@@ -0,0 +1,17 @@
+--[[ keys.lua ]]
+local map = vim.api.nvim_set_keymap
+
+-- remap the key used to leave insert mode
+-- map('i', 'jk', '<ESC>', {})
+--
+-- Toggle nvim-tree
+map('n', 'n', [[:NvimTreeToggle]], {})
+
+-- Toggle more plugins
+map('n', 'l', [[:IndentLinesToggle]], {})
+map('n', 't', [[:TagbarToggle]], {})
+map('n', 'ff', [[:Telescope find_files]], {})
+
+-- Terminal in Window
+map('n', 'w', [[:sp term://zsh]], {})
+map('n', 'w6', [[:res -6]], {})
---
+++ config/lua/plug.lua
@@ -0,0 +1,32 @@
+-- [[ plug.lua ]]
+
+return require('packer').startup(function(use)
+  -- [[ Plugins Go Here ]]
+  use {                                              -- filesystem navigation
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+  }
+
+  -- [[ Theme ]]
+  use { 'mhinz/vim-startify' }                       -- start screen
+  use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+  use {
+    'nvim-lualine/lualine.nvim',                     -- statusline
+    requires = {'kyazdani42/nvim-web-devicons',
+                opt = true}
+  }
+  use { 'Mofiqul/dracula.nvim' }                     -- colorscheme
+
+ -- [[ Dev ]]
+  use {
+    'nvim-telescope/telescope.nvim',                 -- fuzzy finder
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'majutsushi/tagbar' }                        -- code structure
+  use { 'Yggdroot/indentLine' }                      -- see indentation
+  use { 'tpope/vim-fugitive' }                       -- git integration
+  use { 'junegunn/gv.vim' }                          -- commit history
+  use { 'windwp/nvim-autopairs' }                    -- auto close brackets, etc.
+  use {'tpope/vim-commentary'}                       -- comment code
+  use {'neovim/nvim-lspconfig'}                      -- Configurations for Nvim LSP
+end)
